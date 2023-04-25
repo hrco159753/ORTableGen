ffa076390ce7f6044fc8f0a282824be0e71c8e64
Hrvoje Bolešić <hrvojebolesic@hotmail.com>
2023-04-25 21:47:18 +0200

    Fix bash script to be invocation location independent

--- web/run.sh
+++ web/run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-gnome-terminal -- bash -c "python3 -m http.server 8080 --cgi -d $( dirname $0 )"
-gnome-terminal -- bash -c "python3 -m pyftpdlib -w -u user -P user -d $( dirname $0 )/res/ftp"
+BASE_DIR=$( dirname $( readlink -f "${BASH_SOURCE:-$0}" ) )
+echo ${BASE_DIR}
+cd ${BASE_DIR}
+
+gnome-terminal -- bash -c "echo ${BASE_DIR}; python3 -m http.server 8080 --directory ${BASE_DIR} --cgi"
+gnome-terminal -- bash -c "python3 -m pyftpdlib -w -u user -P user -d ${BASE_DIR}/res/ftp"
 
 ip address show
 python -c'import urllib.request as r
 print("External IP:", r.urlopen("https://ident.me").read().decode("utf8"))'
+
+cd -
