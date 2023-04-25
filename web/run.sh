f449e50352fe8f6a138bdedee507c2a7f8dd9f1f
Hrvoje Bolešić <hrvoje.bolesic@aconno.de>
2023-04-25 17:45:13 +0200

    Add local and external IP address output on web server

--- web/run.sh
+++ web/run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-gnome-terminal -- bash -c "echo $( dirname $0 ); python3 -m http.server 8080 --cgi -d $( dirname $0 )"
+gnome-terminal -- bash -c "python3 -m http.server 8080 --cgi -d $( dirname $0 )"
 gnome-terminal -- bash -c "python3 -m pyftpdlib -w -u user -P user -d $( dirname $0 )/res/ftp"
+
+ip address show
+python -c'import urllib.request as r
+print("External IP:", r.urlopen("https://ident.me").read().decode("utf8"))'
