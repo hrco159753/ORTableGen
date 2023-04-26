b3acd77090a177348da5e94f5d541fd0db5e1d12
Hrvoje Bolešić <hrvojebolesic@hotmail.com>
2023-04-26 21:50:02 +0200

    Removed logging of a base directory

--- web/run.sh
+++ web/run.sh
@@ -4,7 +4,7 @@ BASE_DIR=$( dirname $( readlink -f "${BASH_SOURCE:-$0}" ) )
 echo ${BASE_DIR}
 cd ${BASE_DIR}
 
-gnome-terminal -- bash -c "echo ${BASE_DIR}; python3 -m http.server 8080 --directory ${BASE_DIR} --cgi"
+gnome-terminal -- bash -c "python3 -m http.server 8080 --directory ${BASE_DIR} --cgi"
 gnome-terminal -- bash -c "python3 -m pyftpdlib -w -u user -P user -d ${BASE_DIR}/res/ftp"
 
 ip address show
